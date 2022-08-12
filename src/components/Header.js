def59b7963247688fd8b8493b52ffb0c86b56189
s3786643 <s3786643@student.rmit.edu.au>
2022-08-12 17:49:58 +1000

    Apply basic styling to components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
+import './Header.css'
+import Button from 'react-bootstrap/Button';
+
 function Header() {
   return (
-    <div>
+    <div className='header'>
       <h1>Header :)</h1>
+
+      <Button variant="primary">Login</Button>
     </div>
   );
 }
