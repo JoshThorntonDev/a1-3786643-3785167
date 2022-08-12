25f20e44ec89a1dbc8e19e252ff30eb816e51891
s3786643 <s3786643@student.rmit.edu.au>
2022-08-12 20:47:54 +1000

    Make login button show login form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,24 @@
-import './Header.css'
-import Button from 'react-bootstrap/Button';
+import "./Header.css";
+import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate();
+
   return (
-    <div className='header'>
+    <div className="header">
       <h1>Header :)</h1>
 
-      <Button variant="primary">Login</Button>
+      <Button
+        variant="primary"
+        onClick={() => {
+          navigate("/login", {
+            replace: true,
+          });
+        }}
+      >
+        Login
+      </Button>
     </div>
   );
 }
