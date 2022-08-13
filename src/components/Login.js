50d17d071cd409f8c5a635458fc63edf53e7625f
s3786643 <s3786643@student.rmit.edu.au>
2022-08-13 15:18:15 +1000

    Initial login code with hardcoded values

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,46 @@
 import "./Login.css";
+
+// bootstrap for styling
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { FloatingLabel } from "react-bootstrap";
+import Alert from "react-bootstrap/Alert";
+
+// react components for functionality
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const USERNAME = "test@email.com";
+const PASSWORD = "password1";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const attemptLogin = (e) => {
+    setError(""); // clear error in case user has set it already
+    e.preventDefault(); // prevent form from submitting automatically
+
+    if (email === USERNAME && password === PASSWORD) {
+      alert("Logged in");
+    } else {
+      alert("Failure");
+    }
+  };
+
   return (
-    <Form className="mb-3 loginForm">
+    <Form className="mb-3 loginForm" onSubmit={attemptLogin}>
       <h1 className="mb-3 d-flex justify-content-center">Sign In</h1>
       <FloatingLabel label="Email address" className="mb-3">
         <Form.Control
           type="email"
           name="email"
           placeholder="email@example.com"
+          value={email}
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
         />
       </FloatingLabel>
 
@@ -20,6 +49,10 @@ function Login() {
           type="password"
           name="password"
           placeholder="Password here"
+          value={password}
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
         />
       </FloatingLabel>
       <div className="d-flex justify-content-center">
