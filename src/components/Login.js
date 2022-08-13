157f493cac275106e8465b746127aee0c633eb67
s3786643 <s3786643@student.rmit.edu.au>
2022-08-13 15:09:28 +1000

    Remove unused classes; changed to bootstrap classes

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,28 +5,29 @@ import { FloatingLabel } from "react-bootstrap";
 
 function Login() {
   return (
-    <div className="login">
-      <Form className="mb-3 loginForm">
-        <FloatingLabel label="Email address" className="mb-3">
-          <Form.Control
-            type="email"
-            name="email"
-            placeholder="email@example.com"
-          />
-        </FloatingLabel>
+    <Form className="mb-3 loginForm">
+      <h1 className="mb-3 d-flex justify-content-center">Sign In</h1>
+      <FloatingLabel label="Email address" className="mb-3">
+        <Form.Control
+          type="email"
+          name="email"
+          placeholder="email@example.com"
+        />
+      </FloatingLabel>
 
-        <FloatingLabel label="Password" className="mb-3">
-          <Form.Control
-            type="password"
-            name="password"
-            placeholder="Password here"
-          />
-        </FloatingLabel>
+      <FloatingLabel label="Password" className="mb-3">
+        <Form.Control
+          type="password"
+          name="password"
+          placeholder="Password here"
+        />
+      </FloatingLabel>
+      <div className="d-flex justify-content-center">
         <Button variant="primary" type="submit">
           Login
         </Button>
-      </Form>
-    </div>
+      </div>
+    </Form>
   );
 }
 
