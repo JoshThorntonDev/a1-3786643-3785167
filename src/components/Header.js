8204f6a614ebbd48ab408af894d7f755cfdfb2e7
s3786643 <s3786643@student.rmit.edu.au>
2022-08-13 16:13:19 +1000

    Clean Header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import "./Header.css";
 import Button from "react-bootstrap/Button";
 import Nav from "react-bootstrap/Nav";
-import Navbar from 'react-bootstrap/Navbar';
+import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import { useNavigate } from "react-router-dom";
 
@@ -9,47 +9,43 @@ function Header() {
   const navigate = useNavigate();
 
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar className="headerNav" bg="dark" variant="dark">
       <Container>
         <Navbar.Brand>Website Name</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse className="justify-content-end">
-          
-        
-      <Button
-        variant="danger"
-        style={{marginRight: "10px"}}
-        onClick={() => {
-          navigate("/", {
-            replace: true,
-          });
-        }}
-      >
-        Home (temp.)
-      </Button>
+          <Button
+            variant="danger"
+            onClick={() => {
+              navigate("/", {
+                replace: true,
+              });
+            }}
+          >
+            Home (temp.)
+          </Button>
 
-        <Button
-        variant="primary"
-        style={{marginRight: "10px"}}
-        onClick={() => {
-          navigate("/register", {
-            replace: true,
-          });
-        }}
-      >
-        Sign Up
-      </Button>
-        
-        <Button
-        variant="primary"
-        onClick={() => {
-          navigate("/login", {
-            replace: true,
-          });
-        }}
-      >
-        Login
-      </Button>
+          <Button
+            variant="primary"
+            onClick={() => {
+              navigate("/register", {
+                replace: true,
+              });
+            }}
+          >
+            Sign Up
+          </Button>
+
+          <Button
+            variant="primary"
+            onClick={() => {
+              navigate("/login", {
+                replace: true,
+              });
+            }}
+          >
+            Login
+          </Button>
         </Navbar.Collapse>
       </Container>
     </Navbar>
