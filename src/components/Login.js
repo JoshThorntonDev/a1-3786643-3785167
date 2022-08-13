c9afe74b6bc8c459fd18ed0fd7b08afadbae0bf7
s3786643 <s3786643@student.rmit.edu.au>
2022-08-13 15:01:07 +1000

    Styling for login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,30 @@
 import "./Login.css";
 import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { FloatingLabel } from "react-bootstrap";
 
 function Login() {
   return (
     <div className="login">
-      <Form>
-        <Form.Group className="mb-3">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" name="email" placeholder="email" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Password</Form.Label>
+      <Form className="mb-3 loginForm">
+        <FloatingLabel label="Email address" className="mb-3">
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="email@example.com"
+          />
+        </FloatingLabel>
+
+        <FloatingLabel label="Password" className="mb-3">
           <Form.Control
             type="password"
             name="password"
-            placeholder="password"
+            placeholder="Password here"
           />
-        </Form.Group>
+        </FloatingLabel>
+        <Button variant="primary" type="submit">
+          Login
+        </Button>
       </Form>
     </div>
   );
