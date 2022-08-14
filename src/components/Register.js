cf9b14d488af37726935757842d388d96ffa668d
s3785167 <s3785167@student.rmit.edu.au>
2022-08-14 14:39:57 +1000

    Made Register form styling consistent with login

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,28 +1,30 @@
+
+
+// bootstrap for styling
 import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { FloatingLabel } from "react-bootstrap";
+
 
 function Register() {
     return (
-        <div className="register">
-            <h2>Sign Up</h2>
-        <Form>
-        <Form.Group className="mb-3">
-            <Form.Label>Name</Form.Label>
-            <Form.Control type="text" name="name" placeholder="name" />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" name="email" placeholder="email" />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              name="password"
-              placeholder="password"
-            />
-          </Form.Group>
+        <Form className="mb-3 loginForm">
+        <h1 className="mb-3 d-flex justify-content-center">Sign Up</h1>
+        <FloatingLabel label="Name" className="mb-3">
+          <Form.Control type="text" name="name" placeholder="name" />
+        </FloatingLabel>
+        <FloatingLabel label="Email" className="mb-3">
+          <Form.Control type="email" name="email" placeholder="email" />
+        </FloatingLabel>
+        <FloatingLabel label="Password" className="mb-3">
+            <Form.Control type="password" name="password" placeholder="password"/>
+        </FloatingLabel>
+        <div className="d-flex justify-content-center">
+          <Button variant="primary" type="submit">
+              Create Account
+          </Button>
+        </div>
         </Form>
-      </div>
     );
 }
 
