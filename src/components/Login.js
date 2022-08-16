ebde4b6fb0c573a07004a7b2ffd4e65ebc8825a3
s3786643 <s3786643@student.rmit.edu.au>
2022-08-16 19:42:55 +1000

    Implement login code

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,38 +7,41 @@ import FloatingLabel from "react-bootstrap/FloatingLabel";
 
 import AnimatedAlert from "./AnimatedAlert";
 
+import { getUsers } from "../data/Repository"
+
 // react components for functionality
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
-const USERNAME = "test@email.com";
-const PASSWORD = "password1";
 
 function Login({ login }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
   const navigate = useNavigate();
+  const users = getUsers();
+  
 
   const attemptLogin = (e) => {
-    setError(""); // clear error in case user has set it already
+    setError(false); // clear error in case user has set it already
     e.preventDefault(); // prevent form from submitting automatically
-
-    if (email === USERNAME && password === PASSWORD) {
-      login(email);
+    
+    if(email in users && password === users[email]['password']) {
+      login(email)
       navigate("/", { replace: true });
     } else {
-      setError(true);
+      setError(true); // either email or password is wrong
     }
+
+
   };
 
   return (
     <Form className="mb-3 loginForm" onSubmit={attemptLogin}>
       <h1 className="mb-3 d-flex justify-content-center">Sign In</h1>
-
+      
       <AnimatedAlert
         variant="danger"
-        message="Your email and/or password did not match"
+        message="Sorry, your email and/or password did not match our records"
         display={error}
         setDisplay={setError}
       />
