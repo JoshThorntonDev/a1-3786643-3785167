27390a49f0aefa6dcad4ec75a36e96c7f6f5f346
s3786643 <s3786643@student.rmit.edu.au>
2022-08-20 15:35:06 +1000

    Use react bootstrap buttons

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,7 @@
 import "./Profile.css";
-
+import Button from "react-bootstrap/Button";
 import { getUsers } from "../data/Repository";
 import { PencilSquare, PersonCircle, Trash } from "react-bootstrap-icons";
-
 
 function Profile() {
   const users = getUsers();
@@ -10,19 +9,24 @@ function Profile() {
 
   return (
     <div className="profile">
-        
-       <PersonCircle size={'10vh'} className="image"></PersonCircle>
+    
+      <PersonCircle size={"10vh"} className="image"></PersonCircle>
 
-      <div className="information">
+      <div>
         <h1>{users[currentUser].name}'s Profile</h1>
         <p>{users[currentUser].email}</p>
         <hr></hr>
         <p>Joined: {users[currentUser].date}</p>
       </div>
+      <div></div>
       <div className="edit">
-        <div><PencilSquare className="editButton" size={'3vh'}></PencilSquare></div>
-        <div><Trash className="deleteButton" size={'3vh'}></Trash></div>
-        
+        <Button variant="primary" type="submit">
+          <PencilSquare size={"2vh"}></PencilSquare>
+        </Button>
+
+        <Button variant="danger" type="submit">
+          <Trash size={"2vh"}></Trash>
+        </Button>
       </div>
     </div>
   );
