c049e73792f8d3e75cdc6a4b78d7340e16817ffd
s3786643 <s3786643@student.rmit.edu.au>
2022-08-20 14:46:48 +1000

    Improve style of profile page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,8 @@ function Footer() {
   return (
     <footer className="footer">
       <small>
-        Images sourced from <a href="https://unsplash.com/images">Unsplash</a>
+        Images sourced from <a href="https://unsplash.com/images">Unsplash</a> <br />
+        <a href="https://www.flaticon.com/free-icons/user" title="user icons">User icons created by Freepik - Flaticon</a>
       </small>
     </footer>
   );
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,16 +3,28 @@ import "./Profile.css";
 import { getUsers } from "../data/Repository";
 
 function Profile() {
-    const users = getUsers();
-    const currentUser = localStorage.getItem("currentUser");
+  const users = getUsers();
+  const currentUser = localStorage.getItem("currentUser");
 
-    return (
-        <div className="profile">
-            <h1>{users[currentUser].name}'s Profile</h1>
-                <p>{users[currentUser].email}</p>
-                <p>Joined: {users[currentUser].date}</p>
-        </div>
-    );
+  return (
+    <div className="profile">
+      <img
+        className="image"
+        alt={"Placeholder profile"}
+        src="https://cdn-icons.flaticon.com/png/512/552/premium/552848.png?token=exp=1660970412~hmac=cf7a2224dc7e1eb42c971940b696d080"
+      ></img>
+
+      <div className="information">
+        <h1>{users[currentUser].name}'s Profile</h1>
+        <p>{users[currentUser].email}</p>
+        <hr></hr>
+        <p>Joined: {users[currentUser].date}</p>
+      </div>
+      <div className="edit">
+
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
