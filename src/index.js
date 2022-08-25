a849ec5642486c25d2483851847864e1d60f7439
s3786643 <s3786643@student.rmit.edu.au>
2022-08-25 17:20:34 +1000

    Fixed not running initPosts()

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { initUsers } from "./data/Repository.js"
+import { initPosts } from './data/PostRepository';
 
 initUsers();
+initPosts();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
